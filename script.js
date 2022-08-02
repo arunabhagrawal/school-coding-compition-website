dc4861a0a71a4defaa82b9e5aa69221ea18bbf2e
arunabhagrawal <arunabhagrawal@gmail.com>
2022-08-02 18:57:27 +0530

    final project might make some minor changes

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-navList = document.querySelector('.navlist')
-burger.addEventListener('click', () => {
-    navList.classList.toggle('v-class-resp')
-    navbar.classList.toggle('h-nav')
-})
+var currentIndex = 1;
+displaySlides(currentIndex);
+function setSlides(num) {
+    displaySlides(currentIndex += num);
+}
+function displaySlides(num) {
+    var x;
+    var slides = document.getElementsByClassName("imageSlides");
+    if (num > slides.length) { currentIndex = 1 }
+    if (num < 1) { currentIndex = slides.length }
+    for (x = 0; x < slides.length; x++) {
+        slides[x].style.display = "none";
+    }
+    slides[currentIndex - 1].style.display = "block";
+}
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
